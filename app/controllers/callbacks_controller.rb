26779fe38c59331bee0cfbbe1c3481a1f2b66157
cyberplanner <saleh.gfx@gmail.com>
2016-09-07 16:56:31 +0100

    Adds a login notice with github.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    flash[:notice] = "You've successfully signed in with Github"
     sign_in_and_redirect @user
   end
 end
