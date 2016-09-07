96accdf3ad9a53634201b773e4103dc6f37dc067
cyberplanner <saleh.gfx@gmail.com>
2016-09-07 14:03:48 +0100

    :tada: Adds user registration, test passed. closes #1.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -4,10 +4,10 @@ feature 'Signing up' do
   scenario 'user can signup' do
     visit '/'
     click_link 'Sign up'
-    fill_in 'Username', with: "Whatever"
+    fill_in 'User name', with: "Whatever"
     fill_in 'Email', with: "Whatever@email.com"
-    fill_in 'Password', with: "secretsecret7"
-    fill_in 'Password confirmation', with: "secretsecret7"
+    fill_in 'Password', with: "secretsecret7", :match => :prefer_exact
+    fill_in 'Password confirmation', with: "secretsecret7", :match => :prefer_exact
     click_button 'Sign up'
     expect(page).to have_content "Welcome!"
   end
