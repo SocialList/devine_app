904d6c6923ae557ff2804a0ac9c17c8511b528de
cyberplanner <saleh.gfx@gmail.com>
2016-09-08 15:42:25 +0100

    WIP: MailBoxer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    # devise_parameter_sanitizer.for(:sign_up) << :user_name
-    # devise_parameter_sanitizer.for(:account_update) << :user_name
-  end
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  #
+  # protected
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  #   devise_parameter_sanitizer.for(:sign_up) << :user_name
+  #   devise_parameter_sanitizer.for(:account_update) << :user_name
+  # end
 
   rescue_from ActiveRecord::RecordNotFound do
     flash[:warning] = 'Resource not found.'
