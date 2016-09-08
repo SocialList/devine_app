5ec41de0d9b050a419946321b41cc8d1ac46aa65
Frunez <francesco.nunez@gmail.com>
2016-09-08 16:29:42 +0100

    github log out dont log out no

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -9,4 +9,11 @@ feature 'Log in' do
     sign_in
     expect(page).to have_content "Signed in successfully."
   end
+
+  scenario 'user can not see log in/sign up link when logged in' do
+    visit '/'
+    sign_in
+    expect(page).not_to have_link 'Login'
+    expect(page).not_to have_link 'Sign up'
+  end
 end
--- spec/features/logout_spec.rb
+++ spec/features/logout_spec.rb
@@ -4,9 +4,9 @@ feature 'Log out' do
   background do
     user = create(:user)
     visit '/'
-    sign_in
   end
   scenario 'user can signout' do
+    sign_in
     click_link 'Sign out'
     expect(page).to have_content 'Signed out successfully'
   end
